f3203f516890f78dd38456d5c987eea5f04a4b2d
Utku Bektasoglu <utkbktss5@gmail.com>
2023-10-31 19:47:15 +0300

    update Firebase

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -4,12 +4,12 @@ import {getFirestore} from "firebase/firestore"
 import {getStorage} from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDxhsqXvTKVjmAvvdwaB13T9ur5fOua1eA",
-  authDomain: "food-delivery-ab4f3.firebaseapp.com",
-  projectId: "food-delivery-ab4f3",
-  storageBucket: "food-delivery-ab4f3.appspot.com",
-  messagingSenderId: "1026776646683",
-  appId: "1:1026776646683:web:2b6d94e6e04607460424be"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
